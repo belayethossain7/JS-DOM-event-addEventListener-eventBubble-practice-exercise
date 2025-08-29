9e9992524b8823b4566ff7a100c8d327324e0de8
Md. Belayet Hossain <belayet.hossain6847@gmail.com>
2025-08-29 21:52:50 +0600

    add onclick Handler directly or via javascript

--- js/event.js
+++ js/event.js
@@ -1,1 +1,28 @@
 console.log('external')
+
+
+//opton 2 for handle event
+function makePink(){
+            document.body.style.backgroundColor = 'lightpink';
+        }
+
+function makeGreen(){
+            document.body.style.backgroundColor = 'lightgreen';
+        }
+
+
+     
+// option 3 : get element by id and then set onclick
+const btnMakeBlue = document.getElementById('btn-make-blue');
+        btnMakeBlue.onclick = function makeBlue(){
+            document.body.style.backgroundColor = 'lightblue';
+        }
+        // console.log(btnMakeBlue)
+
+
+// option 3 : different version
+const btnMaleAqua = document.getElementById('btn-make-aqua');
+        btnMaleAqua.onclick = makeAqua;
+        function makeAqua(){
+            document.body.style.backgroundColor = 'aqua';
+        }
