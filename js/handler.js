f98bce68e8f7a2e3ca67e39b3ef2bc7d24fbba9f
belayet-hossain <mbelayethossain202@gmail.com>
2025-08-30 09:04:40 +0600

    Integrate event inner text and text update by event event handlers

---
+++ js/handler.js
@@ -0,0 +1,12 @@
+
+document.getElementById('btn-update-title').addEventListener('click', function(){
+            const newTitle = document.getElementById('page-title');
+            newTitle.innerText = 'Belayet Hossain';
+        })
+
+
+document.getElementById('btn-log-in').addEventListener('click', function(){
+            const userInfoEl = document.getElementById('user-info');
+
+            userInfoEl.innerText = 'User: John Doe';
+        })
